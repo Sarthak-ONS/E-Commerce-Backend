905ef864370cbe5e73328f6139a1d33eb88436cc
Sarthak Agarwak <agarwalsarthak456@gmail.com>
2022-12-06 22:41:40 +0530

    Added Filters

--- controllers/product-Controller.js
+++ controllers/product-Controller.js
@@ -27,9 +27,13 @@ exports.addProduct = BigPromise(async (req, res) => {
     }
 
     req.body.photos = imageArray
-
     req.body.user = req.user.id
 
 
+    const product = await Product.create(req.body)
+
+    res.status(200).json({ success: true, product })
 
 })
+
+
---
+++ utils/where-clause.js
@@ -0,0 +1,65 @@
+
+
+
+
+class WhereClause {
+    constructor(base, bigQ) {
+        this.base = base
+        this.bigQ = bigQ
+    }
+
+    search() {
+        const searchWord = this.bigQ.search ? {
+            name: {
+                $regex: this.bigQ.search,
+                $options: 'i'
+            }
+        } : {
+
+        }
+
+        this.base = this.base.find({ ...searchWord })
+        return this
+    }
+
+    page(resultPerPage) {
+        let currentPage = 1
+
+        if (this.bigQ.page) {
+            currentPage = this.bigQ.page
+        }
+
+        const skipValue = resultPerPage * (currentPage - 1)
+        this.base = this.base.limit(resultPerPage).skip(skipValue)
+        return this;
+    }
+
+
+
+    filter() {
+
+        const copyQ = { ...this.bigQ }
+
+        delete copyQ["search"];
+        delete copyQ["limit"];
+        delete copyQ["page"];
+
+
+        //  Convert bigQ into Straing = > copyQ
+
+
+        let stringOfCopyQ = JSON.stringify(copyQ)
+
+        stringOfCopyQ = stringOfCopyQ.replace(/\b(gte|lte|gt|lt)\b/g, m => `$${m}`)
+
+        const jsonOfCopyQ = JSON.parse(stringOfCopyQ)
+
+
+        this.base = this.base.find(jsonOfCopyQ)
+
+    }
+
+
+
+
+}
